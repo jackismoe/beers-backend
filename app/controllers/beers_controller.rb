52797889f4483d4d5168aed2bc21bd7747a00b15
jackismoe <jacksonmo@me.com>
2020-10-15 09:44:01 -0400

    Catch Up

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,2 +1,6 @@
 class BeersController < ApplicationController
+  def index
+    beers = Beer.all
+    render json: beers
+  end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,3 @@
 class Beer < ApplicationRecord
-  has_many :users
-  belongs_to :users
+
 end
