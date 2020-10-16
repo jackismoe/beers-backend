dbf2ae93349a30c988e907b94b688f1cdb2c5950
jackismoe <jacksonmo@me.com>
2020-10-16 13:58:44 -0400

    Build create method for beers; start building out users for unique beer creation

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -8,4 +8,12 @@ class BeersController < ApplicationController
     beer = Beer.find_by(id: params[:id])
     render json: beer
   end
+
+  def create
+    # current_user.beer.create
+
+    beer = Beer.create(id: params[:id], brand: params[:brand], name: params[:name], style: params[:style], hop: params[:hop], yeast: params[:yeast], malts: params[:malts], ibu: params[:ibu], alcohol: params[:alcohol], blg: params[:blg])
+    
+    render json: beer
+  end
 end
