b55e1c56404a11eb6349de4119d4f07dc01f90c3
jackismoe <jacksonmo@me.com>
2020-10-24 13:04:00 -0400

    Change beers#create to create random beer and push to database when requested

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -15,6 +15,7 @@ class BeersController < ApplicationController
   end
 
   def create
+    
     beer = Beer.create(brand: Faker::Beer.brand, name: Faker::Beer.name, style: Faker::Beer.style, hop: Faker::Beer.hop, yeast: Faker::Beer.yeast, malts: Faker::Beer.malts, ibu: Faker::Beer.ibu, alcohol: Faker::Beer.alcohol, blg: Faker::Beer.blg)
     render json: beer
   end
