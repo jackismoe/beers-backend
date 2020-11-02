bbdc00cd439fbe1c52ddaf865fc71a28af233ebd
jackismoe <jacksonmo@me.com>
2020-11-02 10:34:13 -0500

    Validate user during request for profile and fix some elements that load on top of each other

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:email]) || User.find_by(id: params[:session][:user_id])
     if user
       session[:user_id] = user.id
       render json: user
     end
+  end
+
+  def show
+
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   post '/', to: 'sessions#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  post '/current_user', to: 'sessions#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
