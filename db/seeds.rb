b0ac5c7bc3d2307277b3ec1fbad1f3487909afaf
jackismoe <jacksonmo@me.com>
2020-11-07 15:49:24 -0500

    add a fake phone number for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.create(name: 'Jackson', email: 'jack@hey.com', phone: '7069746486', password_digest: '1111')
 User.create(name: 'Lauren', email: 'lkane1499@gmail.com', phone: '', password_digest: '2222')
-User.create(name: 'Kav', email: 'kevin@buyavette.net', phone: '', password_digest: '3333')
+User.create(name: 'Kav', email: 'kevin@buyavette.net', phone: '1234567890', password_digest: '3333')
 User.create(name: 'Kern', email: 'karengrant1011@gmail.com', phone: '', password_digest: '4444')
 User.create(name: 'Smudge', email: 'smudge@cute.org', phone: '7064206969', password_digest: '5555')
 
