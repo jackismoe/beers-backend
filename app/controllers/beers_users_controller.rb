f1e45b7cba27621e462eacaa5fbf4cc06c751f9f
jackismoe <jacksonmo@me.com>
2020-11-11 13:59:43 -0500

    Fix add/remove bug

--- app/controllers/beers_users_controller.rb
+++ app/controllers/beers_users_controller.rb
@@ -1,15 +1,17 @@
 class BeersUsersController < ApplicationController
   def destroy
+    user = User.find_by(id: params[:session])
     x = Beer.find_by(name: params[:beer])
-    y = BeersUser.find_by(beer_id: x.id, user_id: params[:session][:user_id])
+    y = BeersUser.find_by(beer_id: x.id, user_id: user.id)
     y.user_id = 0
     y.save
+    render json: user
   end
 
   def create
     beer = Beer.find_by(name: params[:beer])
-    user = User.find_by(id: params[:session][:user_id])
-    new_beer_user = BeersUser.create(user_id: params[:session][:user_id], beer_id: beer.id)
+    user = User.find_by(id: params[:session])
+    new_beer_user = BeersUser.create(user_id: params[:session], beer_id: beer.id)
 
     new_beer_user.save
     render json: user
