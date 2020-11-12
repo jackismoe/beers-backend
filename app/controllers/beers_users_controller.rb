b8279e2145c5890ba600d536c962567b114967a3
jackismoe <jacksonmo@me.com>
2020-11-12 14:21:05 -0500

    Fix bug with add/remove from log

--- app/controllers/beers_users_controller.rb
+++ app/controllers/beers_users_controller.rb
@@ -1,7 +1,7 @@
 class BeersUsersController < ApplicationController
   def destroy
     user = User.find_by(id: params[:session])
-    x = Beer.find_by(name: params[:beer])
+    x = Beer.find_by(brand: params[:beerBrand], name: params[:beerName])
     y = BeersUser.find_by(beer_id: x.id, user_id: user.id)
     y.user_id = 0
     y.save
@@ -9,9 +9,9 @@ class BeersUsersController < ApplicationController
   end
 
   def create
-    beer = Beer.find_by(name: params[:beer])
-    user = User.find_by(id: params[:session])
-    new_beer_user = BeersUser.create(user_id: params[:session], beer_id: beer.id)
+    beer = Beer.find_by(brand: params[:beerBrand], name: params[:beerName])
+    user = User.find_by(id: params[:session][:user_id])
+    new_beer_user = BeersUser.create(user_id: user.id, beer_id: beer.id)
 
     new_beer_user.save
     render json: user
