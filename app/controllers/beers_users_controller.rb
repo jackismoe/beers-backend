51eb1d18f02b8448e9758ca24d655f3d30d7186c
jackismoe <jacksonmo@me.com>
2020-11-14 11:10:33 -0500

    Prepare for walkthrough

--- app/controllers/beers_users_controller.rb
+++ app/controllers/beers_users_controller.rb
@@ -5,7 +5,7 @@ class BeersUsersController < ApplicationController
     y = Beer.find_by(brand: params[:beerBrand], name: params[:beerName])
     z = BeersUser.find_by(beer_id: y.id, user_id: x.id)
     z.delete
-    render json: x
+    render json: y
   end
 
   def create
