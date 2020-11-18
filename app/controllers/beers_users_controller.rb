2e1a90715bf9167f722da2ce0548d27e52f3401d
jackismoe <jacksonmo@me.com>
2020-11-18 17:15:10 -0500

    Move .save so database doesnt lock

--- app/controllers/beers_users_controller.rb
+++ app/controllers/beers_users_controller.rb
@@ -1,7 +1,9 @@
 class BeersUsersController < ApplicationController
   def destroy
+    user = User.find(params[:session][:user_id])
     beer = BeersUser.find(params[:id])
     beer.user_id = 0
+    render json: user
     beer.save
   end
 
@@ -10,7 +12,7 @@ class BeersUsersController < ApplicationController
     user = User.find_by(id: params[:session][:user_id])
     new_beer_user = BeersUser.create(user_id: user.id, beer_id: beer.id)
 
-    new_beer_user.save
     render json: user
+    new_beer_user.save
   end
 end
