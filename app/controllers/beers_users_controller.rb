5a7cad8c187ef0828e4fbd953eb8e24e5e52170d
jackismoe <jacksonmo@me.com>
2020-11-19 10:10:34 -0500

    Finish oo Refactor

--- app/controllers/beers_users_controller.rb
+++ app/controllers/beers_users_controller.rb
@@ -3,16 +3,19 @@ class BeersUsersController < ApplicationController
     user = User.find(params[:session][:user_id])
     beer = BeersUser.find(params[:id])
     beer.user_id = 0
-    render json: user
-    beer.save
+
+    if beer.save
+      render json: user
+    end
   end
 
   def create
-    beer = Beer.find_by(brand: params[:beerBrand], name: params[:beerName])
+    beer = Beer.find_by(id: params[:beer][:id], brand: params[:beer][:brand])
     user = User.find_by(id: params[:session][:user_id])
     new_beer_user = BeersUser.create(user_id: user.id, beer_id: beer.id)
 
-    render json: user
-    new_beer_user.save
+    if new_beer_user.save
+      render json: user
+    end
   end
 end
